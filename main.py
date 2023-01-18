9b38ea7fc40cc9184187217e84230654386f628b
Pocky <115858316+Pocky-Pancake@users.noreply.github.com>
2023-01-18 17:06:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,29 +17,35 @@ async def doLog(content):
     await log_channel.send(content=content)
 
 @client.event
+async def on_application_command_error(ctx, error):
+    error = getattr(error, "original", error)
+    raise error
+    await doLog(error)
+
+@client.event
 async def on_ready():
     print("bot ready")
 
 @client.event
 async def on_message(message):
     if message.channel.id == test_channel_id and not message.author.bot:
-        print("found message")
+        await doLog("found message")
         f = open("last_regex")
         try:
             regex_match = re.search(f"{f.readline()}", message.content)
             if regex_match:
-                print("found regex match")
+                await doLog("found regex match")
                 embed = nextcord.Embed(title="Match found!", description=f"{regex_match}", color=0x3cb371)
                 embed.add_field(name="Span:", value=f"{regex_match.span()}", inline=False)
                 embed.add_field(name="Group:", value=f"{regex_match.group()}", inline=False)
                 embed.set_footer(text=f"String: {message.content}")
                 await message.channel.send(embed=embed)
             else:
-                print("regex match not found")
+                await doLog("regex match not found")
                 embed = nextcord.Embed(title="Match not found!", description="No match.", color=0xe34234)
                 await message.channel.send(embed=embed)
         except:
-            print("regex match failed")
+            await doLog("regex match failed")
             await message.channel.send("regex match failed")
         f.close()
 
@@ -48,6 +54,7 @@ async def set_regex(interaction:nextcord.Interaction, regex:str = nextcord.Slash
     f = open("last_regex", "w")
     f.write(regex)
     f.close()
+    await doLog(f"Regex set to `{regex}`.")
     await interaction.response.send_message(f"Regex set to `{regex}`.")
 
 @client.slash_command(description="Check current regex checker value.")
@@ -55,6 +62,7 @@ async def check_regex(interaction:nextcord.Interaction):
     f = open("last_regex", "r")
     regex = f.readline()
     f.close()
+    await doLog("/check_regex called.")
     await interaction.response.send_message(f"Current regex is set to `{regex}`.")
 
 client.run(os.getenv('TOKEN'))
